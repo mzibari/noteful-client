16424ca5ac94aeccf052b1e1df894edd27bab0e2
Mahmood Nihad <m.zibari@gmail.com>
2020-07-21 08:03:29 -0500

    preparing for dployment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api'
+  API_ENDPOINT: 'postgres://kketlzitzrnwri:30d5a56504c984ac38ad3c1051c082e12d5ba5362f97418e19c61aec5c60d553@ec2-3-208-50-226.compute-1.amazonaws.com:5432/d91bd5agmefroq',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
